c2c899045aeb713cf41f1c48e22a150f8ccda0bc
MakYamag <ultrasplash@gmail.com>
2022-03-20 22:37:11 +0900

    3/20 Updated.

--- nb/nb011_data_analysis.py
+++ nb/nb011_data_analysis.py
@@ -5,7 +5,7 @@
 # - *Name*を用いて同じ家族を同定し、生存率を計算した新たな列*Family_SurvRate*を作成する。
 # - *Ticket*を用いて上6桁が同じチケット番号のグループ分けし、生存率を計算した新たな列*Ticket_SurvRate*を作成。
 
-# In[1]:
+# In[3]:
 
 
 import numpy as np
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 import string
 
 
-# In[2]:
+# In[4]:
 
 
 #df_train = pd.read_csv('/content/drive/My Drive/Colab Notebooks/data/train.csv')   # Google Colabの場合はこちら
@@ -22,7 +22,7 @@ data_train = pd.read_csv('../data/train.csv')   # ローカルの場合はこち
 data_train.head()
 
 
-# In[3]:
+# In[5]:
 
 
 #df_train = pd.read_csv('/content/drive/My Drive/Colab Notebooks/data/test.csv')   # Google Colabの場合はこちら
@@ -32,7 +32,7 @@ data_test.head()
 
 # ### データ前処理
 
-# In[4]:
+# In[262]:
 
 
 # 'TrainFlag'列追加
@@ -96,6 +96,7 @@ df['Ticket_length'] = df['Ticket'].apply(lambda t: len(str(t)))
 # 'SibSp'+'Parch'+1を新たな列'Family_size'に出力
 df['Family_size'] = df['SibSp'] + df['Parch'] + 1
 
+
 # 'Family_SurvRate': 同じ姓(Surname)を持つ家族内の生存率
 # -------------------------------------------------------
 # Surnameを取り出す関数extract_surnameを定義
@@ -145,7 +146,7 @@ df['Family_SurvRate'] = df['Family_SurvRate'].map(surname_dict)
 # 平均で補完して'Family_SurvRate_NA'列に1を入れる
 df['Family_SurvRate'] = df['Family_SurvRate'].fillna(family_survrate_mean)
 df['Family_SurvRate_NA'] = 0
-df['Family_SurvRate_NA'].loc[df['Family_SurvRate']==family_survrate_mean] = 1
+df['Family_SurvRate_NA'].loc[df['Family_SurvRate']==family_survrate_mean] = 1   # 0:1 = 437:872
 
 
 # 'Ticket_SurvRate': チケット番号が似通っているグループ内の生存率
@@ -193,22 +194,62 @@ df['Ticket_SurvRate'] = df['Ticket_SurvRate'].map(ticketnum_dict)
 # 平均で補完して'Ticket_SurvRate_NA'列に1を入れる
 df['Ticket_SurvRate'] = df['Ticket_SurvRate'].fillna(ticketnum_survrate_mean)
 df['Ticket_SurvRate_NA'] = 0
-df['Ticket_SurvRate_NA'].loc[df['Ticket_SurvRate']==ticketnum_survrate_mean] = 1
+df['Ticket_SurvRate_NA'].loc[df['Ticket_SurvRate']==ticketnum_survrate_mean] = 1   # 0:1 = 470:839
+
+
+# 'Deck': 'Cabin'欠損値（Z）を他の特徴量から予測する
+# ---------------------------------------------------
+# 'Deck'を予測するデータ（元々Zだったデータ）はわかるように'Deck_predict'列に1を入れる
+df['Deck_predict'] = 0
+df['Deck_predict'].loc[df['Deck']=='Z'] = 1   # 0:1 = 295:1014
+
+### 'Deck'予測用データ成形 ###
+df_Deck = df.drop(['Survived', 'TrainFlag'], axis=1)
+
+# 'Sex'、'Embarked'、'Title'、'Ticket_first'をone-hot-encodeする
+df_Deck_oh = pd.get_dummies(df_Deck[['Sex', 'Embarked', 'Title', 'Ticket_first']], drop_first=True)
+
+# one-hot-encodeデータを結合する
+df_Deck_added = pd.concat([df_Deck, df_Deck_oh], axis=1)
+
+# 'Name'、'Sex'、'Ticket'、'Cabin'、'Embarked'、'Title'、'Ticket_first'、'Surname'、'Ticket_num'を削除する
+df_Deck_deleted = df_Deck_added.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked', 'Title', 'Ticket_first',
+                                                      'Surname', 'Ticket_num'], axis=1)
+# 'Deck'予測用のtrainデータとtest（予測）データに分離
+df_Deck_X_train = df_Deck_deleted[df_Deck_deleted['Deck_predict']==0].drop(['Deck_predict', 'Deck'], axis=1)
+df_Deck_y_train = df_Deck_deleted[df_Deck_deleted['Deck_predict']==0]['Deck']
+df_Deck_X_test = df_Deck_deleted[df_Deck_deleted['Deck_predict']==1].drop(['Deck_predict', 'Deck'], axis=1)
+
+# Deck_X、Deck_yとしてNumpy配列にする
+Deck_X_train = df_Deck_X_train.values
+Deck_y_train = df_Deck_y_train.values
+Deck_X_test = df_Deck_X_test.values
+
+### 'Deck'予測モデル ###
+from sklearn.ensemble import RandomForestClassifier
+
+rf_deck = RandomForestClassifier(criterion='gini', n_estimators=100, random_state=21, n_jobs=-1)
+rf_deck.fit(Deck_X_train, Deck_y_train)
+new_Deck = rf_deck.predict(Deck_X_test)
+
+### 'Deck'予測データに置き換え ###
+df['new_Deck'] = df['Deck']
+df['new_Deck'].loc[df['Deck_predict']==1] = new_Deck
 
 
 # =========
 # 特徴量整理
 # =========
 
-# 'Sex'、'Embarked'、'Deck'、'Title'、'Ticket_first'をone-hot-encodeする
-df_oh = pd.get_dummies(df[['Sex', 'Embarked', 'Deck', 'Title', 'Ticket_first']], drop_first=True)
+# 'Sex'、'Embarked'、'new_Deck'、'Title'、'Ticket_first'をone-hot-encodeする
+df_oh = pd.get_dummies(df[['Sex', 'Embarked', 'new_Deck', 'Title', 'Ticket_first']], drop_first=True)
 
 # one-hot-encodeデータを結合する
 df_added = pd.concat([df, df_oh], axis=1)
 
-# 'Name'、'Sex'、'Ticket'、'Cabin'、'Embarked'、'Deck'、'Title'、'Ticket_first'、'Surname'、'Ticket_num'を削除する
-df_deleted = df_added.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked', 'Deck', 'Title', 'Ticket_first', 'Surname', 'Ticket_num'],
-                           axis=1)
+# 'Name'、'Sex'、'Ticket'、'Cabin'、'Embarked'、'Deck'、'Title'、'Ticket_first'、'Surname'、'Ticket_num', 'new_Deck'を削除する
+df_deleted = df_added.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked', 'Deck', 'Title', 'Ticket_first',
+                            'Surname', 'Ticket_num', 'new_Deck'], axis=1)
 df_deleted.info()
 
 # 訓練用とテスト用に再び分割
@@ -227,7 +268,7 @@ print('X_test:', X_test.shape)
 
 # ### データ解析
 
-# In[7]:
+# In[224]:
 
 
 # 訓練用、テスト用にデータ分割する
@@ -240,7 +281,7 @@ print('Label counts in y_train: [0 1] =', np.bincount(y_train.astype(np.int64)))
 print('Label counts in y_train_test: [0 1] =', np.bincount(y_train_test.astype(np.int64)))
 
 
-# In[8]:
+# In[226]:
 
 
 # =================================
@@ -252,7 +293,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.pipeline import make_pipeline
 
-pl_svc = make_pipeline(StandardScaler(), SVC(random_state=21, max_iter=5000))
+pl_svc = make_pipeline(StandardScaler(), SVC(random_state=21, max_iter=10000))
 
 from sklearn.model_selection import GridSearchCV
 
@@ -270,7 +311,7 @@ print('Test accuracy: %f' % svc_bestclf.score(X_train_test, y_train_test))
 svc_pred = svc_bestclf.predict(X_test)
 
 
-# In[9]:
+# In[227]:
 
 
 # =============================================
@@ -303,7 +344,7 @@ print('Test accuracy: %f' % rf_bestclf.score(X_train_test, y_train_test))
 rf_pred = rf_bestclf.predict(X_test)
 
 
-# In[10]:
+# In[228]:
 
 
 # =============================================
@@ -333,7 +374,7 @@ print('Test accuracy: %f' % ada_bestclf.score(X_train_test, y_train_test))
 ada_pred = ada_bestclf.predict(X_test)
 
 
-# In[11]:
+# In[229]:
 
 
 # ========================
@@ -348,4 +389,22 @@ vote_clf.fit(X_train, y_train)
 print('Test accuracy: %f' % vote_clf.score(X_train_test, y_train_test))
 
 vote_pred = vote_clf.predict(X_test)
+
+
+# In[326]:
+
+
+cabin_plot_before = df.groupby(df['Deck'])['Deck'].count()
+cabin_plot_after = df.groupby(df['new_Deck'])['new_Deck'].count()
+
+cabin_plot = pd.concat([cabin_plot_before, cabin_plot_after], axis=1, join='outer')
+cabin_plot
+
+fig = plt.figure()
+plt.bar(cabin_plot.index, cabin_plot.iloc[:, 1], width=0.5, color='g', label='After', align='edge')
+plt.bar(cabin_plot.index, cabin_plot.iloc[:, 0], width=0.5, color='c', label='Before')
+plt.legend()
+
+# プロット画像保存
+plt.savefig('../image/nb011_Deck.png')
 
